568a439903b71d79eebb045c7247fb12fec69179
Tulio <tulio.luz@bloomingdales.com>
2019-02-26 13:33:30 -0300

    created structure of the project

---
+++ src/test/java/utils/SingletonBrowserClass.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SingletonBrowserClass {
+
+    // instance of singleton class
+    private static SingletonBrowserClass instanceOfSingletonBrowserClass=null;
+
+    private WebDriver driver;
+
+    // Constructor
+    private SingletonBrowserClass(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        driver= new ChromeDriver();
+    }
+
+    // TO create instance of class
+    public static SingletonBrowserClass getInstanceOfSingletonBrowserClass(){
+        if(instanceOfSingletonBrowserClass==null){
+            instanceOfSingletonBrowserClass = new SingletonBrowserClass();
+        }
+        return instanceOfSingletonBrowserClass;
+    }
+
+    // To get driver
+    public WebDriver getDriver()
+    {
+        return driver;
+    }
+}
---
+++ src/test/java/utils/TestRunner.java
@@ -0,0 +1,12 @@
+package utils;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/java/features/", glue="")
+
+public class TestRunner {
+
+}
